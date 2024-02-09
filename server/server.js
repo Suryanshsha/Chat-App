72e4894456a82a9cc2bc1e5e22549decd0129b54
Suryanshsha <gillbroking325@gmail.com>
2024-02-09 13:31:58 +0530

    creating client and server port

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-import express from "express";
+import express from "express"
+import {Server} from 'socket.io'
+import {createServer } from 'http'
+
+const port = 3000
+
+const app = express()
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
 
 
-const app = express();
+io.on("connection", (socket) => {
+    console.log("User Connected");
+    console.log("Id", socket.id);
+})
 
-
-
-
-
-app.listen(3000, () => {
-    
-    console.log("Surver is running on port 3000");
+app.listen(port, () => {
+  console.log(`Surver is running on port ${port}`)
 })
